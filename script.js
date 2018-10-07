5616f8ec11ab3c3f84c51b744d738c145abacbdc
Jeffrey <phelpsjeffrey@yahoo.com>
2018-10-07 10:19:55 -0600

    Random colors addition

--- script.js
+++ script.js
@@ -4,14 +4,7 @@ const squares = document.querySelectorAll(".square");
 const colorDisplay = document.getElementById("colorDisplay");
 const messageDisplay = document.querySelector("#message");
 
-let colors = [
-    'rgb(255, 0, 0)', 
-    'rgb(255, 255, 0)', 
-    'rgb(0, 255, 0)', 
-    'rgb(0, 255, 255)', 
-    'rgb(0, 0, 255)', 
-    'rgb(255, 0, 255)'
-];
+let colors = generateRandomColors(6);
 
 let pickedColor = pickColor();
 
@@ -28,18 +21,32 @@ for(let i = 0; i < colors.length; i++) {
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
-        }
+        };
     });
 };
 
 function changeColors(color){
     for(let i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = color;
-    }
-}
+    };
+};
 
 function pickColor(){
     let randomColor = Math.floor(Math.random() * colors.length);
     return colors[randomColor];
 };
 
+function generateRandomColors(num){
+    let arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+};
+
+function randomColor(){
+    let reds = Math.floor(Math.random() * 256);
+    let greens = Math.floor(Math.random() * 256);
+    let blues = Math.floor(Math.random() * 256);
+    return "rgb(" + reds + ", " + greens + ", " + blues + ")";
+};
